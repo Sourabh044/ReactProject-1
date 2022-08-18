76887a6d66ddf9ae3dace647e614ca7d27d51213
Sourabh <45278177+Sourabh044@users.noreply.github.com>
2022-08-18 15:03:40 +0530

    Beta

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, 
-         Routes,
-         Route, } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
-import About from './components/About';
-import TextForm from './components/TextForm';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
